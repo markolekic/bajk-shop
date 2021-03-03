c5de95db73963b4c2201b6dbb621f999ef01cd0a
Marko Lekic <markolekic985@gmail.com>
2021-03-03 09:37:42 +0100

    navbar fix onscroll color change

--- js/js.js
+++ js/js.js
@@ -1,17 +1,28 @@
+const navbar = document.querySelector(".navbar");
+const textBlock = document.querySelector('#text-block')
 const navbarToggler = document.querySelector(".navbar-toggler");
 const navbarMenu = document.querySelector(".navbar ul");
-const navbarLinks = document.querySelectorAll(".navbar a");
 
+//Navbar toggle
 navbarToggler.addEventListener("click", () => {
-    navbarToggler.classList.toggle("open-navbar-toggler");
-    navbarMenu.classList.toggle("active");
+  navbarToggler.classList.toggle("open-navbar-toggler");
+  navbarMenu.classList.toggle("active");
+});
+
+//Onscroll color change
+window.addEventListener("scroll", () => {
+  if (window.scrollY > textBlock.offsetTop) {
+    navbar.style.backgroundColor = 'white'
+  } else {
+    navbar.style.backgroundColor = 'transparent'
+  }
 });
 
 
 //Init swiper
-    var swiper = new Swiper('.swiper-container', {
-      pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-      },
-    });
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    dynamicBullets: true,
+  },
+});
